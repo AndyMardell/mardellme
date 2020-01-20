c84758a06d85da856c651c52b92d9b30b2aa3a10
Andy Mardell <andy@sidigital.co>
2020-01-20 14:24:50 +0000

    Small margin tweak on mobile

--- components/global/Heading/Subtitle.tsx
+++ components/global/Heading/Subtitle.tsx
@@ -5,13 +5,14 @@ const StyledDiv = styled.h2`
   font-family: 'Montserrat', sans-serif;
   font-weight: 800;
   text-transform: uppercase;
-  font-size: calc(30px + (120 - 30) * ((100vw - 300px) / (1600 - 300)));
+  font-size: calc(33px + (120 - 33) * ((100vw - 300px) / (1600 - 300)));
   letter-spacing: 0.05em;
-  text-shadow: 7px 9px 0px rgba(70, 70, 70, 1);
-  margin: 1.5em 0;
+  text-shadow: 4px 5px 0px rgba(70, 70, 70, 1);
+  margin: 2em 0 1.5em;
 
   @media only screen and (min-width: 900px) {
     margin: 0.5em 0;
+    text-shadow: 7px 9px 0px rgba(70, 70, 70, 1);
   }
 `
 
