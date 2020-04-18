2f9e83c1f44907075215b8b48f2278142f11b442
Andy Mardell <andy@sidigital.co>
2020-04-19 00:40:42 +0100

    Remove analyzer causing bugs

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+module.exports = withPlugins([optimizedImages], {
+  env: {
+    SPOTIFY_ID: process.env.SPOTIFY_ID,
+    SPOTIFY_SECRET: process.env.SPOTIFY_SECRET,
+    SPOTIFY_REDIRECT: process.env.SPOTIFY_REDIRECT,
+    FAUNADB_SECRET: process.env.FAUNADB_SECRET,
+  },
 })
-
-module.exports = withBundleAnalyzer(
-  withPlugins([optimizedImages], {
-    env: {
-      SPOTIFY_ID: process.env.SPOTIFY_ID,
-      SPOTIFY_SECRET: process.env.SPOTIFY_SECRET,
-      SPOTIFY_REDIRECT: process.env.SPOTIFY_REDIRECT,
-      FAUNADB_SECRET: process.env.FAUNADB_SECRET,
-    },
-  })
-)
