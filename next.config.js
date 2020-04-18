6a6e6357f7846af292cbeb1fec248c912c43c342
Andy Mardell <andy@sidigital.co>
2020-04-18 16:45:00 +0100

    Add webpack analyzer

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
 
-module.exports = withPlugins([optimizedImages], {
-  env: {
-    SPOTIFY_ID: process.env.SPOTIFY_ID,
-    SPOTIFY_SECRET: process.env.SPOTIFY_SECRET,
-    SPOTIFY_REDIRECT: process.env.SPOTIFY_REDIRECT,
-    FAUNADB_SECRET: process.env.FAUNADB_SECRET,
-  },
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 })
+
+module.exports = withBundleAnalyzer(
+  withPlugins([optimizedImages], {
+    env: {
+      SPOTIFY_ID: process.env.SPOTIFY_ID,
+      SPOTIFY_SECRET: process.env.SPOTIFY_SECRET,
+      SPOTIFY_REDIRECT: process.env.SPOTIFY_REDIRECT,
+      FAUNADB_SECRET: process.env.FAUNADB_SECRET,
+    },
+  })
+)
