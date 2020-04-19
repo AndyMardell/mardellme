18000498971c7a8beeab3b509c4814802d6cd501
Andy Mardell <andy@sidigital.co>
2020-04-19 17:47:27 +0100

    Pointless bump

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import App from 'next/app'
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
-
 import { theme } from '../styles'
 
 export default class MyApp extends App {
