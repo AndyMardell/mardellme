731ff5d66495baa0812736252ff1a138f4e72f89
Andy Mardell <andy@sidigital.co>
2024-03-16 15:55:28 +0000

    Use next/font to load local fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import localFont from 'next/font/local'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Container from '@/components/global/Container'
@@ -16,9 +17,55 @@ interface Props {
   children: React.ReactNode
 }
 
+const montserrat = localFont({
+  src: '../fonts/montserrat-extrabold-webfont.woff2',
+  display: 'swap',
+  variable: '--font-montserrat'
+})
+
+const plexSans = localFont({
+  src: [
+    {
+      path: '../fonts/ibmplexsans-light-webfont.woff2',
+      weight: '300',
+      style: 'normal'
+    },
+    {
+      path: '../fonts/ibmplexsans-lightitalic-webfont.woff2',
+      weight: '300',
+      style: 'italic'
+    },
+    {
+      path: '../fonts/ibmplexsans-medium-webfont.woff2',
+      weight: '500',
+      style: 'normal'
+    },
+    {
+      path: '../fonts/ibmplexsans-mediumitalic-webfont.woff2',
+      weight: '500',
+      style: 'italic'
+    },
+    {
+      path: '../fonts/ibmplexsans-semibold-webfont.woff2',
+      weight: '600',
+      style: 'normal'
+    },
+    {
+      path: '../fonts/ibmplexsans-bold-webfont.woff2',
+      weight: '700',
+      style: 'normal'
+    }
+  ],
+  display: 'swap',
+  variable: '--font-plex-sans'
+})
+
 export default function RootLayout({ children }: Readonly<Props>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${montserrat.variable} ${plexSans.variable}`}
+    >
       <body>
         <Container>
           <Header />
--- src/components/spotify/Playing.tsx
+++ src/components/spotify/Playing.tsx
@@ -3,10 +3,10 @@ import style from '@/styles/spotify/Playing.module.scss'
 export default function Playing() {
   return (
     <div className={style.music}>
-      <span className="line1" />
-      <span className="line2" />
-      <span className="line3" />
-      <span className="line4" />
+      <span />
+      <span />
+      <span />
+      <span />
     </div>
   )
 }
