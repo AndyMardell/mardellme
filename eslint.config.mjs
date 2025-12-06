156d76b70a94b319fc214629295e28d56ecf8331
Andy Mardell <andy@sidigital.co>
2025-12-06 17:58:49 +0000

    Upgrade Next.js and fix scss issues

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import prettier from "eslint-plugin-prettier";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: [...nextCoreWebVitals, ...compat.extends("prettier")],
+
+    plugins: {
+        prettier,
+    },
+
+    rules: {
+        "prettier/prettier": "error",
+    },
+}]);
